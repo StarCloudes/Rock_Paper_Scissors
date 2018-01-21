cce5f1101819bd0d8c9c85f435b10803fd01b73c
starcloudes <starcloudes@gmail.com>
2018-01-21 11:49:26 +0800

    修改window窗口

--- main.js
+++ main.js
@@ -13,7 +13,12 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 700,
+    transparent: true,
+    frame: false
+  })
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
